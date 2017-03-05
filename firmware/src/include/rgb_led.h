32df8b4f567a230bb19dd1ac71f7afd55d9273ab
Morgan Stuart <morgansstuart243@gmail.com>
2017-03-04 19:23:01 -0500

    Separate out LED functions

---
+++ firmware/src/include/rgb_led.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   rgb_led.h
+ * Author: morgan
+ *
+ * Created on March 4, 2017, 6:31 PM
+ */
+
+#ifndef RGB_LED_H
+#define	RGB_LED_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+void red(int val);
+
+void green(int val);
+
+void blue(int val);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* RGB_LED_H */
+
