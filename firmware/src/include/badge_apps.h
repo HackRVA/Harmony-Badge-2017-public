b3695b10a91c3498b2d1e61455d2930664249e94
Morgan Stuart <morgansstuart243@gmail.com>
2017-03-04 19:24:16 -0500

    example app task and structure
    badge_apps/hello_world_app.c defines code similar to what was in the
    init function (set LED and write to screen).
    
    badge_apps.h contains app declarations needed globally, such as the
    task function.
    
    This structure is just a first pass

---
+++ firmware/src/include/badge_apps.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   badge_apps.h
+ * Author: morgan
+ *
+ * Created on March 4, 2017, 6:33 PM
+ */
+
+// Create one header that declares everything needed to create/use
+// badge application tasks
+
+#ifndef BADGE_APPS_H
+#define	BADGE_APPS_H
+
+// Declare tasks
+void hello_world_task(void* p_arg);
+
+//Declare other functions or structures that can be used across apps?
+
+#endif	/* HELLO_WORLD_APP_H */
+
