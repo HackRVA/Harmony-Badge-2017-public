bbd131ebf6e4ba188922b4f7cc65794a28509610
Morgan Stuart <morgansstuart243@gmail.com>
2017-05-28 15:25:42 -0400

    led declaration timer1_int header; clear rgb_led.c/h

--- firmware/src/include/timer1_int.h
+++ firmware/src/include/timer1_int.h
@@ -3,6 +3,7 @@
 void red(unsigned char onPWM) ;
 void green(unsigned char onPWM) ;
 void blue(unsigned char onPWM) ;
+void led(unsigned char r, unsigned char g, unsigned char b);
 void backlight(unsigned char bright) ;
 void timerInit();
 
