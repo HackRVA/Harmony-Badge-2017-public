cf638c225be39850ba9502aca879692baad7ad38
Morgan Stuart <morgansstuart243@gmail.com>
2017-06-04 10:43:55 -0400

    Add app declarations

--- firmware/src/include/badge_apps.h
+++ firmware/src/include/badge_apps.h
@@ -18,8 +18,16 @@ void badgey_bird_task(void* p_arg);
 
 void badgelandia_task(void* p_arg);
 
+void groundwar_task(void* p_arg);
+
 void screensaver_task(void* p_arg);
 
 void boot_splash_task(void *p_arg);
+
+void blinkenlights_task(void *args);
+
+void conductor_task(void *args);
+
+void dice_roll_task(void* p_arg);
 #endif
 
